2f8c5285f4af4b5b50fa0743e4b22bffe42dc7fb
DfrancoisP <40683284+DfrancoisP@users.noreply.github.com>
2019-01-31 10:11:02 -0800

    Add files via upload
    Palindrome test

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palin = "leel";
+            string drone = "drone";
+            CheckPalin(palin);
+            CheckPalin(drone);
+        }
+
+        static void CheckPalin(string s)
+        {
+            bool isPal = true;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                int j = s.Length - 1 - i;
+                if (s[i]!= s[j])
+                {
+                    isPal = false;
+                    break;
+                }
+                if (i>=j)
+                {                 
+                    break;
+                }
+                
+            }
+            if (isPal)
+            {
+                Console.WriteLine("isPal");
+            }
+            else
+            { 
+                Console.WriteLine("not pal");
+            }
+        }
+    }
+}
